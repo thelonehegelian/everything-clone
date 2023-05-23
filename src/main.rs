f723cdab46a1c7a46b66fda8ef4b96d7d03f2ac2
thelonehegelian <thelonehegelian@gmail.com>
2023-05-23 20:24:41 +0100

    Add BTree map for file indexing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,75 @@
-use std::{collections::HashMap, time::SystemTime};
+use std::fs::File;
+use std::path::PathBuf;
+use std::{collections::BTreeMap, collections::HashMap, time::SystemTime};
+
 // use std::fs::Metadata;s
+use std::time::{Duration, Instant};
 use walkdir::WalkDir;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    struct FileMetadata {
-        name: String,
-        size: u64,
-        created: SystemTime,
-        modified: SystemTime,
-        accessed: SystemTime,
+    let start = Instant::now();
+    // create_b_index();
+    create_hash_index();
+    println!("Indexing in Progress...");
+    let duration = start.elapsed();
+    println!("Time taken: {} ms", duration.as_millis());
+    Ok(())
+}
+#[derive(Debug)]
+struct FileMetadata {
+    name: String,
+    size: u64,
+    created: SystemTime,
+    modified: SystemTime,
+    accessed: SystemTime,
+}
+type Path = String;
+fn create_b_index() -> BTreeMap<PathBuf, FileMetadata> {
+    let mut b_index: BTreeMap<PathBuf, FileMetadata> = BTreeMap::new();
+
+    for entry in WalkDir::new("../.").into_iter().filter_map(|e| e.ok()) {
+        if entry.file_type().is_file() {
+            let path = entry.path().to_path_buf();
+
+            let metadata = entry.metadata().unwrap();
+            let accessed = metadata
+                .accessed()
+                .unwrap_or_else(|_| SystemTime::UNIX_EPOCH);
+            let created = metadata
+                .created()
+                .unwrap_or_else(|_| SystemTime::UNIX_EPOCH);
+            let modified = metadata
+                .modified()
+                .unwrap_or_else(|_| SystemTime::UNIX_EPOCH);
+            b_index.insert(
+                path.clone(),
+                FileMetadata {
+                    name: entry.file_name().to_string_lossy().to_string(),
+                    size: metadata.len(),
+                    created,
+                    modified,
+                    accessed,
+                },
+            );
+        }
+        // println!("{:?}", entry);
     }
 
-    // @todo update to use PathBuf
-    type Path = String;
-
-    // create a hash map of files to their metadata
+    b_index
+}
+fn create_hash_index() -> HashMap<String, FileMetadata> {
     let mut files: HashMap<Path, FileMetadata> = HashMap::new();
-
-    let mut path = "../../../vuejs";
-    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new("../.").into_iter().filter_map(|e| e.ok()) {
         // make sure we only get files
         if entry.file_type().is_file() {
-            path = entry.path().to_str().unwrap();
+            let path = entry.path().to_str().unwrap();
+
             //@note metadata is a method provided by `walkdir::DirEntry`
             // @note also possible with fsmetadata(filename)
-            let metadata = entry.metadata()?;
+            let metadata = entry.metadata().unwrap();
             let accessed = metadata
                 .accessed()
                 .unwrap_or_else(|_| std::time::SystemTime::UNIX_EPOCH);
-            // @todo update this value
-            let size = metadata.len();
             let created = metadata
                 .created()
                 .unwrap_or_else(|_| std::time::SystemTime::UNIX_EPOCH);
@@ -40,6 +80,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 path.to_string(),
                 FileMetadata {
                     name: entry.file_name().to_str().unwrap().to_string(),
+                    // @todo update this value
                     size: metadata.len(),
                     created: created,
                     modified: modified,
@@ -48,6 +89,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             );
         }
     }
-
-    Ok(())
+    files
 }
