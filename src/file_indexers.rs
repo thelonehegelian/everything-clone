309ee35d1953ab03aea0daba79c6b1fc14f3fb2c
thelonehegelian <thelonehegelian@gmail.com>
2023-05-28 20:31:47 +0100

    Add a create_hash_index using rayon

--- src/file_indexers.rs
+++ src/file_indexers.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use std::collections::{BTreeMap, HashMap};
 use std::path::Path;
 use std::path::PathBuf;
@@ -16,7 +17,7 @@ pub struct FileMetadata {
 pub fn create_hash_index() -> HashMap<String, FileMetadata> {
     let mut files: HashMap<String, FileMetadata> = HashMap::new();
 
-    for entry in WalkDir::new("../.").into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new("../..").into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
             let path = entry.path().to_str().unwrap();
             let metadata = entry.metadata().unwrap();
@@ -77,4 +78,34 @@ pub fn create_b_index() -> BTreeMap<PathBuf, FileMetadata> {
     }
 
     b_index
+}
+
+// @note almost twice as fast as the sequential version, especially on large directories
+pub fn create_hash_index_parallel() -> HashMap<String, FileMetadata> {
+    let files: HashMap<String, FileMetadata> = WalkDir::new("../..")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .par_bridge() // Convert iterator to a parallel iterator
+        .filter(|entry| entry.file_type().is_file())
+        .map(|entry| {
+            let path = entry.path().to_str().unwrap();
+            let metadata = entry.metadata().unwrap();
+            let accessed = metadata.accessed().unwrap_or(SystemTime::UNIX_EPOCH);
+            let created = metadata.created().unwrap_or(SystemTime::UNIX_EPOCH);
+            let modified = metadata.modified().unwrap_or(SystemTime::UNIX_EPOCH);
+
+            (
+                path.to_string(),
+                FileMetadata {
+                    name: entry.file_name().to_string_lossy().to_string(),
+                    size: metadata.len(),
+                    created,
+                    modified,
+                    accessed,
+                },
+            )
+        })
+        .collect();
+
+    files
 }
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ use walkdir::WalkDir;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = Instant::now();
     // create_b_index();
-    let file_index = file_indexers::create_hash_index();
-    println!("{:?}", file_index[".././web_assembly/game-of-life/www/node_modules/es-abstract/2016/OrdinaryGetOwnProperty.js"]);
+    // let file_index = file_indexers::create_hash_index();
+    let file_index = file_indexers::create_hash_index_parallel();
+    // println!("{:?}", file_index[".././web_assembly/game-of-life/www/node_modules/es-abstract/2016/OrdinaryGetOwnProperty.js"]);
     println!("Indexing in Progress...");
     let duration = start.elapsed();
     println!("Time taken: {} ms", duration.as_millis());
