4dc60af5dbc8e7f6e7abed737e69b2c55cb7052d
thelonehegelian <thelonehegelian@gmail.com>
2023-06-01 22:26:42 +0100

    Load file_index from a cache file

--- src/cache_file_index.rs
+++ src/cache_file_index.rs
@@ -1,19 +1,56 @@
 use crate::file_indexers::create_hash_index_parallel;
+use crate::file_metadata::FileMetadata;
 use serde_json;
 use std::collections::HashMap;
+use std::fs;
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::{self, BufRead, Write};
 
-pub fn create_and_cache_file_index() -> Result<(), Box<dyn std::error::Error>> {
+pub fn create_and_cache_file_index(
+) -> Result<HashMap<String, FileMetadata>, Box<dyn std::error::Error>> {
+    match fs::metadata("file_index_cache.json") {
+        Ok(metadata) if metadata.is_file() => {
+            println!("Loading cached file index from file_index_cache.json");
+            return load_cached_file_index();
+        }
+        Ok(_) => {
+            println!("file_index_cache.json found, but it is not a regular file");
+        }
+        Err(ref e) if e.kind() == std::io::ErrorKind::NotFound => {
+            println!("file_index_cache.json not found");
+        }
+        Err(e) => {
+            return Err(e.into());
+        }
+    }
+
+    println!("Creating file index and caching it in file_index_cache.json");
     let file_index = create_hash_index_parallel();
 
     // Convert the file index to JSON
-    // @todo use a serialized file format instead of JSON
     let json = serde_json::to_string(&file_index)?;
 
     // Write the JSON to a file
-    let mut file = File::create("file_index.json")?;
+    let mut file = File::create("file_index_cache.json")?;
     file.write_all(json.as_bytes())?;
 
-    Ok(())
+    Ok(file_index)
+}
+
+fn load_cached_file_index() -> Result<HashMap<String, FileMetadata>, Box<dyn std::error::Error>> {
+    // Check if the file exists
+    // @todo uncomment this after creating a command line argument to create the file index
+    // if !fs::metadata("file_index.json")?.is_file() {
+    //     return Err("File index not found".into());
+    // }
+
+    // Read the JSON from the file
+    let file = File::open("file_index_cache.json")?;
+    let reader = io::BufReader::new(file);
+    let json: String = reader.lines().collect::<Result<_, _>>()?;
+
+    // Parse the JSON into the file index hashmap
+    let file_index: HashMap<String, FileMetadata> = serde_json::from_str(&json)?;
+
+    Ok(file_index)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,17 @@ mod file_searchers;
 use file_searchers::{display_results, file_search};
 mod cache_file_index;
 use cache_file_index::create_and_cache_file_index;
+use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = Instant::now();
     // create_b_index();
     // let file_index = file_indexers::create_hash_index();
-    let file_index = file_indexers::create_hash_index_parallel();
-    create_and_cache_file_index()?;
-    println!("File Cache Created!");
+    // let file_index = file_indexers::create_hash_index_parallel();
+    let file_index = create_and_cache_file_index()?;
+    // let file_index = load_cached_file_index()?;
 
     // count the number of files in the index
     println!("Number of files indexed: {}", file_index.len());
